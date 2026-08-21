3bcc278d9e1847aecfb5cfe0469442d766e573ba
Sven Göthel <sgothel@jausoft.com>
2026-08-22 01:16:52 +0200

    iptables: tightening limits + refine print-network-stats
    tightening limits
    - global syn drop 10/s -> 6s global (have stats for 1/s + 3/s)
    - https  syn drop  8/s -> 5s
    - https  conn 10 -> 5
    
    refine print-network-stats
    - proper grouping Netstat, Process, IP Buckets
    - add git, ssh netstat
    - add total of IP Buckets
    - simplify filtering iptable list by line-number

--- config/server/xx-attack/scripts/print-network-stats.sh
+++ config/server/xx-attack/scripts/print-network-stats.sh
@@ -6,39 +6,54 @@ top -bn1 | head | grep Cpu | awk ' { printf "CPU %3d%, ", 100-$8 }' ; uptime | s
 free -h
 echo
 
-printf "total conn %6d, syn  %6d \t\tlisten-drops %6d\t\t\t\t(Netstat)\n" \
+printf "total conn %6d, syn  %6d \t\tlisten-drops %6d\t\t\t\t(Netstat Current)\n" \
     $(ss -tan state established | wc -l) \
     $(ss -tan state syn-recv | wc -l) \
     $(netstat -s | grep "SYNs" | awk '{ print $1 }')
 
-printf "https conn %6d, syn  %6d \t\tsyn-flood  %6d 2/s, %d 6/s, %d above\t\t(syn-flood IPs)\n" \
+printf "time-wait  %6d \t\t\tclose-wait %6d \t\tssh %2d, syn %d\n" \
+    $(ss -tan state time-wait | wc -l) \
+    $(ss -tan state close-wait | wc -l) \
+    $(ss -tan state established | grep :22 | wc -l) \
+    $(ss -tan state syn-recv | grep :22 | wc -l)
+
+printf "https conn %6d, syn  %6d \t\thttp conn  %6d, syn  %d \tgit %2d, syn %d\n" \
     $(ss -tan state established | grep :443 | wc -l) \
     $(ss -tan state syn-recv | grep :443 | wc -l) \
-    $(cat /proc/net/ipt_hashlimit/synflood2 | wc -l) \
-    $(cat /proc/net/ipt_hashlimit/synflood6 | wc -l) \
-    $(cat /proc/net/ipt_hashlimit/synflood | wc -l)
+    $(ss -tan state established | grep :80 | wc -l) \
+    $(ss -tan state syn-recv | grep :80 | wc -l) \
+    $(ss -tan state established | grep :9418 | wc -l) \
+    $(ss -tan state syn-recv | grep :9418 | wc -l)
 
-#printf "http  conn %6d, syn  %6d \t\tssh conn   %6d, syn %6d\n" \
-#    $(ss -tan state established | grep :80 | wc -l) \
-#    $(ss -tan state syn-recv | grep :80 | wc -l) \
-#    $(ss -tan state established | grep :22 | wc -l) \
-#    $(ss -tan state syn-recv | grep :22 | wc -l)
-
-printf "time-wait  %6d \t\t\tclose-wait %6d\n" \
-    $(ss -tan state time-wait | wc -l) \
-    $(ss -tan state close-wait | wc -l)
-
-printf "cgit #     %6d \t\t\thttpd #    %6d\n" \
+printf "cgit #     %6d \t\t\thttpd #    %6d\t\t\t\t(Process Count)\n" \
     $(ps ax | grep cgit.cgi | wc -l) \
     $(ps ax | grep apache2 | wc -l)
 
 echo
 
-printf "https ipv4 %6d, ipv6 %6d \t\thttp ipv4  %6d, ipv6 %6d\t\t\t(IPs)\n" \
+printf "https ipv4 %6d, ipv6 %6d \t\thttp ipv4  %6d, ipv6 %d\t\t\t(IP Buckets: %6d total)\n" \
     $(cat /proc/net/ipt_hashlimit/https-limit | wc -l) \
     $(cat /proc/net/ip6t_hashlimit/https-limit | wc -l) \
     $(cat /proc/net/ipt_hashlimit/http-limit | wc -l) \
-    $(cat /proc/net/ip6t_hashlimit/http-limit | wc -l)
+    $(cat /proc/net/ip6t_hashlimit/http-limit | wc -l) \
+    $((  $(cat /proc/net/ipt_hashlimit/https-limit | wc -l) \
+       + $(cat /proc/net/ip6t_hashlimit/https-limit | wc -l) \
+       + $(cat /proc/net/ipt_hashlimit/http-limit | wc -l) \
+       + $(cat /proc/net/ip6t_hashlimit/http-limit | wc -l) ))
+
+printf "syn   ipv4 %6d 1/s, %4d 3/s, %d > 6/s, ipv6  %6d 1/s, %d 3/s, %d > 6/s\t\t(IP Buckets: %6d total)\n" \
+    $(cat /proc/net/ipt_hashlimit/synflood1 | wc -l) \
+    $(cat /proc/net/ipt_hashlimit/synflood3 | wc -l) \
+    $(cat /proc/net/ipt_hashlimit/synflood | wc -l) \
+    $(cat /proc/net/ip6t_hashlimit/synflood1 | wc -l) \
+    $(cat /proc/net/ip6t_hashlimit/synflood3 | wc -l) \
+    $(cat /proc/net/ip6t_hashlimit/synflood | wc -l) \
+    $((  $(cat /proc/net/ipt_hashlimit/synflood1 | wc -l) \
+       + $(cat /proc/net/ipt_hashlimit/synflood3 | wc -l) \
+       + $(cat /proc/net/ipt_hashlimit/synflood | wc -l) \
+       + $(cat /proc/net/ip6t_hashlimit/synflood1 | wc -l) \
+       + $(cat /proc/net/ip6t_hashlimit/synflood3 | wc -l) \
+       + $(cat /proc/net/ip6t_hashlimit/synflood | wc -l) ))
 
 #printf "ssh   ipv4 %6d, ipv6 %6d\n" \
 #    $(cat /proc/net/ipt_hashlimit/ssh-limit | wc -l) \
@@ -47,16 +62,16 @@ printf "https ipv4 %6d, ipv6 %6d \t\thttp ipv4  %6d, ipv6 %6d\t\t\t(IPs)\n" \
 echo
 
 echo "ipv4  dos_limiter"
-iptables -L dos_limiter -n -v | grep -v -E "icmptype (0$|3$|3 code 4$|11$|12$|13$)|syn_flood" | tail -n +2
+iptables -L dos_limiter -n -v --line-numbers | grep -v -E "^2 |^3 |^4 |^5 |^6 |^7 |^10 " | tail -n +2
 echo
 echo "ipv6  dos_limiter"
-ip6tables -L dos_limiter -n -v | grep -v -E "ipv6-icmptype (1$|2$|3$|4$|129$|130.*$|131.*$|132.*$|133.*$|134.*$|135.*$|136.*$|143.*$|148.*$|149.*$|153.*$)|syn_flood" | tail -n +3
+ip6tables -L dos_limiter -n -v --line-numbers | grep -v -E "^2 |^3 |^4 |^5 |^6 |^7 |^9 |^10 |^13 " | tail -n +2
 echo
 
 echo "ipv4  syn-flood"
-iptables -L syn_flood -n -v | tail -n +3
+iptables -L syn_flood -n -v --line-numbers | tail -n +3
 echo
 echo "ipv6  syn-flood"
-ip6tables -L syn_flood -n -v | tail -n +3
+ip6tables -L syn_flood -n -v --line-numbers | tail -n +3
 echo
 
